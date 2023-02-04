5e835d6d832bf1dd7bd2ca7ab4263b13e59df90a
Wahyu Siregar <wahyuusiregarr@gmail.com>
2023-02-04 11:39:30 +0700

    commit all file

---
+++ ASD KLP 3 Closest Pair/src/asd/klp/pkg3/closest/pair/ASDKLP3ClosestPair.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package asd.klp.pkg3.closest.pair;
+
+/**
+ *
+ * @author wahyu
+ */
+public class ASDKLP3ClosestPair {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new ClosestPair().setVisible(true);
+    }
+    
+}
